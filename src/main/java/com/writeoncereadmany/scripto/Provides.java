0c2cfc9db1ee6f85143f529940ed6aa0035c643b
tom <tarjohnson1979@gmail.com>
2017-10-08 16:43:01 +0100

    Managed to hook up requires and provides annotations

--- src/main/java/com/writeoncereadmany/scripto/Provides.java
+++ src/main/java/com/writeoncereadmany/scripto/Provides.java
@@ -1,5 +1,9 @@
 package com.writeoncereadmany.scripto;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Provides {
     String[] value();
 }
--- src/main/java/com/writeoncereadmany/scripto/ScriptoRunner.java
+++ src/main/java/com/writeoncereadmany/scripto/ScriptoRunner.java
@@ -3,15 +3,50 @@ package com.writeoncereadmany.scripto;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.util.Arrays;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ScriptoRunner implements BeforeTestExecutionCallback {
 
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
-        Requires requirements = context.getRequiredTestMethod().getDeclaredAnnotation(Requires.class);
+        Method testMethod = context.getRequiredTestMethod();
+        Class testClass = context.getRequiredTestClass();
+        Object testInstance = context.getRequiredTestInstance();
+
+        Requires requirements = testMethod.getDeclaredAnnotation(Requires.class);
         if(requirements != null) {
-            throw new RuntimeException("Requires " + Arrays.toString(requirements.value()));
+            List<Method> setup = getRequirements(testClass, requirements.value());
+            for(Method method : setup) {
+                method.invoke(testInstance);
+            }
         }
+    }
+
+    private List<Method> getRequirements(Class<?> context, String... requirements) {
+        return Stream.of(requirements).map(req -> getRequirement(context, req)).collect(toList());
+    }
+
+    private Method getRequirement(Class<?> testClass, String requirement) {
+        return Stream
+            .of(testClass.getMethods())
+            .filter(provides(requirement))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Could not find method providing " + requirement));
+    }
+
+    private Predicate<Method> provides(String requirement) {
+        return method -> {
+            Provides provides = method.getDeclaredAnnotation(Provides.class);
+            if(provides != null) {
+                return Stream.of(provides.value()).anyMatch(requirement::equals);
+            } else {
+                return false;
+            }
+        };
     }
 }
